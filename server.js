ef81109b7b872952daafa138d43dfe754e1e4a42
meriem26 <mmlys89@gmail.com>
2019-07-15 17:39:06 +0100

    Fix port issue

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ let albumsData = [
       "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0"
   }
 ];
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("My app is running here!");
 });
